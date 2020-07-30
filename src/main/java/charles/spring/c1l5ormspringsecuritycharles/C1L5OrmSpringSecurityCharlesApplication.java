ea84029709b2d9d3fa5ac90108bf7204ce6bc729
Charles <dailiang18bb@gmail.com>
2020-07-30 19:33:45 -0400

    Project init

---
+++ src/main/java/charles/spring/c1l5ormspringsecuritycharles/C1L5OrmSpringSecurityCharlesApplication.java
@@ -0,0 +1,13 @@
+package charles.spring.c1l5ormspringsecuritycharles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class C1L5OrmSpringSecurityCharlesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(C1L5OrmSpringSecurityCharlesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/charles/spring/c1l5ormspringsecuritycharles/C1L5OrmSpringSecurityCharlesApplicationTests.java
@@ -0,0 +1,13 @@
+package charles.spring.c1l5ormspringsecuritycharles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class C1L5OrmSpringSecurityCharlesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
